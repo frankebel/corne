1d39a610d5e779e656fc865344a39886acf5bed6
Frank Ebel <git@ebel.page>
2025-05-13 20:13:02 +0200

    refactor: specify microcontroller

--- keyboards/splitkb/aurora/corne/keymaps/frankebel/rules.mk
+++ keyboards/splitkb/aurora/corne/keymaps/frankebel/rules.mk
@@ -5,4 +5,6 @@ RGBLIGHT_ENABLE = no
 # Caps Word
 CAPS_WORD_ENABLE = yes
 
-CONVERT_TO = promicro_rp2040
+# converters https://docs.qmk.fm/feature_converters
+PIN_COMPATIBLE = promicro
+CONVERT_TO = helios
